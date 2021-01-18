d3a5281776a1f456db47dfc59811c8b7fc8b1611
Mathieu Ibersien <mathieu.ibersien@etu.univ-lehavre.fr>
2021-01-18 17:04:08 +0100

    Init du front qui est un projet react de base pour le moment. Début du code du back.

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 const express = require('express')
-
 const app = express();
 
-app.listen('3000', () => {
+const SerialPort = require('serialport')
+const Readline = require('@serialport/parser-readline')
+const port = new SerialPort('/dev/ttyACM0')
+
+const parser = port.pipe(new Readline({
+    delimiter: '\r\n'
+}))
+
+const onOpen = () => {
+    console.log("On open...")
+}
+
+const onData = (data) => {
+    console.log("data: " + data);
+}
+
+parser.on('open', onOpen);
+parser.on('data', onData);
+
+app.listen('4000', () => {
     console.log("Server Started !")
 })
--- serial-port.js
+++ serial-port.js
@@ -1,9 +1,8 @@
 const SerialPort = require('serialport')
-const Readline = require('@serialport.parser-readline')
-const port = new SerialPort('dev/ttyACM0') //list with ls /dev/tty*
+const Readline = require('@serialport/parser-readline')
+const port = new SerialPort('/dev/ttyACM0')
 
 const parser = port.pipe(new Readline({
     delimiter: '\r\n'
-}));
-
+}))
 parser.on('data', console.log)
